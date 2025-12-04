b99770e2a77952a2f68a5fe510e7a644c214277c
KUMAR RISHAB <kumarrishab@KUMARs-MacBook-Air.local>
2025-12-04 18:05:23 +0530

    added initial files

---
+++ week-2/springwebtutorial/src/main/java/com/example/cohort/springwebtutorial/SpringwebtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.cohort.springwebtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebtutorialApplication.class, args);
+	}
+
+}
---
+++ week-2/springwebtutorial/src/test/java/com/example/cohort/springwebtutorial/SpringwebtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cohort.springwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
